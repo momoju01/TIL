c43c47f0b786ee336aed0bde4c007718c8d66a1b
김윤하 <beupthere@gmail.com>
2021-09-14 23:52:35 +0900

    0914 DB - sqlite/orm

--- Django/0905/django_exercise/movies/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Movie
-
-# Register your models here.
-class Admin_movie(admin.ModelAdmin):
-    list_display =('title', 'overview', 'poster_path')
-
-admin.site.register(Movie, Admin_movie)
-
--- Django/0909/code_practice/articles/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Article
-
-class ArticleForm(forms.ModelForm):
-
-    class Meta: # form에 대한 정보
-        model = Article
-        fields = '__all__'
