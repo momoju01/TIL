2292d5fd8178699dcd7e8d2d084f614d18f610eb
김윤하 <beupthere@gmail.com>
2021-11-08 23:24:19 +0900

    1108 javascript & vuejs

---
+++ vuejs/1108/02_vue_cli/my-first-app/src/components/About.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <h1>About</h1>
+    <h2>{{ myMessage }}</h2>
+    <h2>{{ parentData }}</h2>
+    <input 
+      type="text"
+      @keyup.enter="childInputChange"
+      v-model="childInputData" 
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name:'About',
+  props: {
+    myMessage: String,
+    parentData: String,
+  },
+  methods: {
+    childInputChange: function () {
+      this.$emit('child-input-change', this.childInputData)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vuejs/1108/02_vue_cli/my-first-app/src/views/TheLotto.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>로또 번호 추첨</h1>
+    <h2>{{ $route.params.lottoNum }}개의 번호를 추첨합니다.</h2>
+    <button @click="getLucnkyNum">픽!!</button>
+    <p>오늘의 로또 번호</p>
+    <p>{{ selectedLuckyNum }}</p>
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'
+
+export default {
+  name: 'TheLotto',
+  data: function () {
+    return {
+      selectedLuckyNum: [],
+    }
+  },
+  methods: {
+    getLucnkyNum: function () {
+      const numbers = _.range(1, 46)
+      this.selectedLuckyNum = _.sampleSize(numbers, this.$route.params.lottoNum)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
