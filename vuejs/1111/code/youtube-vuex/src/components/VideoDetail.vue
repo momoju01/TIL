08c93d72c7da6ee48174d111c4217ba7eca231a2
김윤하 <beupthere@gmail.com>
2021-11-12 20:00:00 +0900

    1112 vuejs

---
+++ vuejs/1111/code/youtube-vuex/src/components/VideoDetail.vue
@@ -0,0 +1,44 @@
+<template>
+  <div v-if="isSelected">
+    <iframe :src="videoUrl" frameborder="0"></iframe>
+    <h2>{{ videoTitle }}</h2>
+    <h2>{{ videoDesc }}</h2>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+
+export default {
+  name: 'VideoDetail',
+  computed: {
+    ... mapGetters([
+      'isSelected',
+      'videoUrl',
+      'videoTitle',
+      'videoDesc',
+    ])
+
+
+
+    // isSelected: function () {
+      // 가공하고싶을 때는 이걸로 가공해서 쓰기
+    //   return this.$store.getters.isSelected
+    // },
+    // videoUrl: function () {
+    //   return this.$store.getters.videoUrl
+    // },
+    // videoTitle: function () {
+    //   return this.$store.getters.videoTitle
+    // },
+    // videoDesc: function () {
+    //   return this.$store.getters.videoDesc
+    // }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vuejs/1111/code/youtube-vuex/src/components/VideoListItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <li @click="selectVideo">
+      <img :src="videoImg" alt="">
+      <span>{{ videoTitle }}</span>
+    </li>
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'
+
+export default {
+  name: 'VideoListItem',
+  props: {
+    video: Object,
+  },
+  methods:{
+    selectVideo: function () {
+      // console.log('비디오 선택됨')
+      this.$store.dispatch('selectVideo', this.video)
+    }
+  },
+  computed: {
+    videoTitle: function () {
+      return _.unescape(this.video.snippet.title)
+    },
+    videoImg: function () {
+      return this.video.snippet.thumbnails.default.url
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
