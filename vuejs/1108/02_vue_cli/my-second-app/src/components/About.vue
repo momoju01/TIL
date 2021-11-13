c838906eb36a813fb3c5c29bc3a5ca9fe7585ce3
김윤하 <beupthere@gmail.com>
2021-11-13 20:19:13 +0900

    1113 vuejs - router

---
+++ vuejs/1108/02_vue_cli/my-second-app/src/components/About.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>About</h1>
+    <h2>{{ myMessage }}</h2>
+    <h2>{{ parentData }}</h2>
+    <input 
+      type="text"
+      @keyup.enter="childInputChange"
+      v-model="childInputData"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  props: {
+    myMessage: String,
+    parentData: String,
+  },
+  data: function () {
+    return {
+      childInputData: null,
+    }
+  },
+  methods: {
+    childInputChange: function () {
+      this.$emit('child-input-change', this.childInputData)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
