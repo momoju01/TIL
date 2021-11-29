8319e4965492861c3c64dd9c72f52488cc22000f
김윤하 <beupthere@gmail.com>
2021-11-29 21:24:25 +0900

    1129 db update

--- Db/1126/99_mtm_practice/hospitals/models.py
+++ Db/1126/99_mtm_practice/hospitals/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Doctor(models.Model):
     name = models.TextField()
 
@@ -10,7 +9,15 @@ class Doctor(models.Model):
 
 class Patient(models.Model):
     name = models.TextField()
-    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    # 외래키 삭제
 
     def __str__(self):
         return f'{self.pk}번 환자 {self.name}'
+
+# 중개모델 작성
+class Reservation(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.doctor_id}번 의사의 {self.patient_id}번 환자'
