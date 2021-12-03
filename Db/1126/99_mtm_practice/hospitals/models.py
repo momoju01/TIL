e0ae70b6b4aa39c9e475a4c1188cc705ac413044
김윤하 <beupthere@gmail.com>
2021-12-03 21:04:54 +0900

    1203 db update

--- Db/1126/99_mtm_practice/hospitals/models.py
+++ Db/1126/99_mtm_practice/hospitals/models.py
@@ -6,19 +6,18 @@ class Doctor(models.Model):
     def __str__(self):
         return f'{self.pk}번 의사 {self.name}'
 
-
 class Patient(models.Model):
+    doctors = models.ManyToManyField(Doctor, through='Reservation')
     name = models.TextField()
-    # ManyToManyField - related_name 작성
-    doctors = models.ManyToManyField(Doctor, through='Reservation', related_name='patients')
 
     def __str__(self):
         return f'{self.pk}번 환자 {self.name}'
 
-
 class Reservation(models.Model):
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    symptom = models.TextField()
+    reserved_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.doctor_id}번 의사의 {self.patient_id}번 환자'
+        return f'{self.doctor.pk}번 의사의 {self.patient.pk}번 환자'
