8f22e65569cfafd9fa61189c5b18faf937d890e1
김윤하 <beupthere@gmail.com>
2021-12-11 09:16:23 +0900

    django RESTful API

--- Django/Advanced/1208/00_json_response/ssafy.py
+++ Django/Advanced/1208/00_json_response/ssafy.py
@@ -2,4 +2,7 @@ import requests
 from pprint import pprint
 
 response = requests.get('http://127.0.0.1:8000/api/v1/json-3/')
-print(response.json())
+# pprint(type(response.json()))
+
+data = response.json()
+pprint(data[0])
