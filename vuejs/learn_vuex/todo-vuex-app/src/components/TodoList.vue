250555230c68396eaa8eb7cb6d568a5f48d12c65
김윤하 <beupthere@gmail.com>
2022-01-03 18:04:39 +0900

    study: create todo-vuex-app

---
+++ vuejs/learn_vuex/todo-vuex-app/src/components/TodoList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <todo-list-item></todo-list-item>
+  </div>
+</template>
+
+<script>
+import TodoListItem from '@/components/TodoListItem'
+
+export default {
+  name: 'TodoList',
+  components: {
+    TodoListItem,
+  }
+}
+</script>
+
+<style>
+
+</style>
